3bbcfed6ffc2da724cf07f8372c004fb00181507
Akash Shridhar Balsaraf <127662333+AKASHSBALSARAF@users.noreply.github.com>
2023-07-28 23:24:57 +0530

    Update epsiIBM.py
    def get_boundaries + def fix_bugs ... try the code ,i have doubts about fix_bugs function

--- epsiIBM.py
+++ epsiIBM.py
@@ -21,26 +21,23 @@ def obj_count(array, dim, shape):
         return nobjz
 
 def get_boundaries(array, dim, shape, lx, ly, lz, nobjmax, dd):
-
     if dim == 'x':
         xi = np.zeros((nobjmax, shape[1], shape[2]))
         xf = np.zeros((nobjmax, shape[1], shape[2]))
 
         for k in range(shape[2]):
             for j in range(shape[1]):
-                inum = 0
-                if array[0, j, k]:
-                    xi[inum, j, k] = -dd[0]
+                mask = array[:, j, k]
+                diff = np.diff(mask)
 
-                for i in range(shape[0] - 1):
-                    if not array[i, j, k] and array[i + 1, j, k]:
-                        xi[inum, j, k] = dd[1] * i + dd[1] / 2
-                    elif array[i, j, k] and not array[i + 1, j, k]:
-                        xf[inum, j, k] = dd[1] * i + dd[1] / 2
-                        inum += 1
+                start_indices = np.where(diff == 1)[0] + 1
+                end_indices = np.where(diff == -1)[0]
 
-                if array[-1, j, k]:
-                    xf[inum, j, k] = lx + dd[0]
+                if mask[0]:
+                    xi[0, j, k] = -dd[0]
+                xi[1:len(start_indices) + 1, j, k] = (start_indices - 0.5) * dd[1]
+                xf[:len(end_indices) + 1, j, k] = (end_indices + 0.5) * dd[1]
+                xf[len(end_indices) + 1:, j, k] = lx + dd[0]
 
         return xi, xf
 
@@ -50,195 +47,97 @@ def get_boundaries(array, dim, shape, lx, ly, lz, nobjmax, dd):
 
         for k in range(shape[2]):
             for i in range(shape[0]):
-                jnum = 0
-                if array[i, 0, k]:
-                    yi[jnum, i, k] = -dd[2]
+                mask = array[i, :, k]
+                diff = np.diff(mask)
 
-                for j in range(shape[1] - 1):
-                    if not array[i, j, k] and array[i, j + 1, k]:
-                        yi[jnum, i, k] = dd[3] * j + dd[3] / 2
-                    elif array[i, j, k] and not array[i, j + 1, k]:
-                        yf[jnum, i, k] = dd[3] * j + dd[3] / 2
-                        jnum += 1
+                start_indices = np.where(diff == 1)[0] + 1
+                end_indices = np.where(diff == -1)[0]
 
-                if array[i, -1, k]:
-                    yf[jnum, i, k] = ly + dd[2]
+                if mask[0]:
+                    yi[0, i, k] = -dd[2]
+                yi[1:len(start_indices) + 1, i, k] = (start_indices - 0.5) * dd[3]
+                yf[:len(end_indices) + 1, i, k] = (end_indices + 0.5) * dd[3]
+                yf[len(end_indices) + 1:, i, k] = ly + dd[2]
 
         return yi, yf
 
-    else:
+    elif dim == 'z':
         zi = np.zeros((nobjmax, shape[0], shape[1]))
         zf = np.zeros((nobjmax, shape[0], shape[1]))
 
         for j in range(shape[1]):
             for i in range(shape[0]):
-                knum = 0
-                if array[i, j, 0]:
-                    zi[knum, i, j] = -dd[4]
+                mask = array[i, j, :]
+                diff = np.diff(mask)
 
-                for k in range(shape[2] - 1):
-                    if not array[i, j, k] and array[i, j, k + 1]:
-                        zi[knum, i, j] = dd[5] * k + dd[5] / 2
-                    elif array[i, j, k] and not array[i, j, k + 1]:
-                        zf[knum, i, j] = dd[5] * k + dd[5] / 2
-                        knum += 1
+                start_indices = np.where(diff == 1)[0] + 1
+                end_indices = np.where(diff == -1)[0]
 
-                if array[i, j, -1]:
-                    zf[knum, i, j] = ly + dd[4]
+                if mask[0]:
+                    zi[0, i, j] = -dd[4]
+                zi[1:len(start_indices) + 1, i, j] = (start_indices - 0.5) * dd[5]
+                zf[:len(end_indices) + 1, i, j] = (end_indices + 0.5) * dd[5]
+                zf[len(end_indices) + 1:, i, j] = ly + dd[4]
 
         return zi, zf
 
-def fixBugs(ep,rafep,dim,shape,nobj,nobjraf, nraf, nobjmax, dimi, dimf):
-    ibugs = np.where(np.not_equal(nobj,nobjraf))
-    print(ibugs)
-    if(dim=='x'):
-        for l in len(ibugs[0]):
-            iobj = -1
-
-            if (ep[0,ibugs[0][l],ibugs[1][l]]):
-                iobj += 1 
-
-            for i in range(shape[0]-1):
-
-                if(not ep[i,ibugs[0][l],ibugs[1][l]] and ep[i+1,ibugs[0][l],ibugs[1][l]]):
-                    iobj += 1
-
-                elif(not ep[i,ibugs[0][l],ibugs[1][l]] and not ep[i+1,ibugs[0][l],ibugs[1][l]]):
-                    iflu = 1
-
-                elif(ep[i,ibugs[0][l],ibugs[1][l]] and ep[i+1,ibugs[0][l],ibugs[1][l]]):
-                    isol = 1
-
-                for iraf in range(nraf-1):
-
-                    if not rafep[iraf+nraf*(i), ibugs[0][l], ibugs[1][l]] and rafep[iraf+nraf*(i)+1, ibugs[0][l], ibugs[1][l]]:
-                        idebraf = iraf + nraf*(i)
-
-                    if rafep[iraf+nraf*(i), ibugs[0][l], ibugs[1][l]] and not rafep[iraf+nraf*(i)+1, ibugs[0][l], ibugs[1][l]]:
-                        ifinraf = iraf + nraf*(i)
-
-                if idebraf != 0 and ifinraf != 0 and idebraf < ifinraf and iflu == 1:
-                    iobj += 1
-
-                    for ii in range(iobj, nobjmax-1):
-
-                        dimi[ii,ibugs[0][l], ibugs[1][l]] = dimi[ii+1,ibugs[0][l], ibugs[1][l]]
-                        dimf[ii,ibugs[0][l], ibugs[1][l]] = dimf[ii+1,ibugs[0][l], ibugs[1][l]] 
-
-                    iobj -= 1
-
-                if idebraf != 0 and ifinraf != 0 and idebraf > ifinraf and isol == 1:
-                    iobj += 1
-                    for ii in range(iobj, nobjmax-1):
-                        dimi[ii,ibugs[0][l],ibugs[1][l]] = dimi[ii+1,ibugs[0][l],ibugs[1][l]]
-                    
-                    iobj -= 1
-                    for ii in range(iobj, nobjmax-1):
-                        dimf[ii,ibugs[0][l],ibugs[1][l]] = dimf[ii+1,ibugs[0][l],ibugs[1][l]]
-
-                idebraf = 0
-                ifinraf = 0
-                iflu = 0
-                isol = 0
-
-    elif(dim=='y'):
-        for l in len(ibugs[0]):
-            iobj = -1
-
-            if (ep[ibugs[0][l],0,ibugs[1][l]]):
-                iobj += 1 
-
-            for j in range(shape[1]-1):
-
-                if(not ep[ibugs[0][l],j,ibugs[1][l]] and ep[ibugs[0][l],j+1,ibugs[1][l]]):
-                    iobj += 1
-
-                elif(not ep[ibugs[0][l],j,ibugs[1][l]] and not ep[ibugs[0][l],j+1,ibugs[1][l]]):
-                    iflu = 1
-
-                elif(ep[ibugs[0][l],j,ibugs[1][l]] and ep[ibugs[0][l],j+1,ibugs[1][l]]):
-                    isol = 1
-
-                for iraf in range(nraf-1):
-
-                    if not rafep[ibugs[0][l], iraf+nraf*(j), ibugs[1][l]] and rafep[ibugs[0][l], iraf+nraf*(j)+1, ibugs[1][l]]:
-                        idebraf = iraf + nraf*(j)
-
-                    if rafep[ibugs[0][l], iraf+nraf*(j-1), ibugs[1][l]] and not rafep[ibugs[0][l], iraf+nraf*(j)+1, ibugs[1][l]]:
-                        ifinraf = iraf + nraf*(j)
-
-                if idebraf != 0 and ifinraf != 0 and idebraf < ifinraf and iflu == 1:
-                    iobj += 1
-
-                    for ii in range(iobj, nobjmax-1):
-
-                        dimi[ii,ibugs[0][l], ibugs[1][l]] = dimi[ii+1,ibugs[0][l], ibugs[1][l]]
-                        dimf[ii,ibugs[0][l], ibugs[1][l]] = dimf[ii+1,ibugs[0][l], ibugs[1][l]] 
-
-                    iobj -= 1
-
-                if idebraf != 0 and ifinraf != 0 and idebraf > ifinraf and isol == 1:
-                    iobj += 1
-                    for ii in range(iobj, nobjmax-1):
-                        dimi[ii,ibugs[0][l],ibugs[1][l]] = dimi[ii+1,ibugs[0][l],ibugs[1][l]]
-                    
-                    iobj -= 1
-                    for ii in range(iobj, nobjmax-1):
-                        dimf[ii,ibugs[0][l],ibugs[1][l]] = dimf[ii+1,ibugs[0][l],ibugs[1][l]]
-
-                idebraf = 0
-                ifinraf = 0
-                iflu = 0
-                isol = 0
-
     else:
-        for l in len(ibugs[0]):
-            iobj = -1
+        raise ValueError("Invalid dimension. Supported dimensions are 'x', 'y', and 'z'.")
 
-            if (ep[ibugs[0][l],ibugs[1][l],0]):
-                iobj += 1 
+import numpy as np
 
-            for k in range(shape[2]-1):
+def fixBugs(ep, rafep, dim, shape, nobj, nobjraf, nraf, nobjmax, dimi, dimf):
+    ibugs = np.where(np.not_equal(nobj, nobjraf))
+    
+    for l in range(len(ibugs[0])):
+        i, j, k = ibugs[0][l], ibugs[1][l], ibugs[2][l]
 
-                if(not ep[ibugs[0][l],ibugs[1][l],k] and ep[ibugs[0][l],ibugs[1][l],k+1]):
-                    iobj += 1
+        if dim == 'x':
+            dim_arr = dimi[:, i, j]
+            dim_arr_f = dimf[:, i, j]
+            ep_dim = ep[:, i, j]
+            rafep_dim = rafep[:, i, j]
+        elif dim == 'y':
+            dim_arr = dimi[:, i, j]
+            dim_arr_f = dimf[:, i, j]
+            ep_dim = ep[i, :, j]
+            rafep_dim = rafep[i, :, j]
+        elif dim == 'z':
+            dim_arr = dimi[:, i, j]
+            dim_arr_f = dimf[:, i, j]
+            ep_dim = ep[i, j, :]
+            rafep_dim = rafep[i, j, :]
+        else:
+            raise ValueError("Invalid dimension. Supported dimensions are 'x', 'y', and 'z'.")
 
-                elif(not ep[ibugs[0][l],ibugs[1][l],k] and not ep[ibugs[0][l],ibugs[1][l],k+1]):
-                    iflu = 1
+        iobj = -1
+        if ep_dim[0]:
+            iobj += 1
 
-                elif(ep[ibugs[0][l],ibugs[1][l],k] and ep[ibugs[0][l],ibugs[1][l],k+1]):
-                    isol = 1
+        for idx in range(shape[0] - 1):
+            if not ep_dim[idx] and ep_dim[idx + 1]:
+                iobj += 1
 
-                for iraf in range(nraf-1):
+        if dim == 'x':
+            rafep_dim = rafep_dim[:nraf * (shape[0] - 1)]
+        elif dim == 'y':
+            rafep_dim = rafep_dim[:nraf * (shape[1] - 1)]
+        elif dim == 'z':
+            rafep_dim = rafep_dim[:nraf * (shape[2] - 1)]
 
-                    if not rafep[ibugs[0][l], ibugs[1][l], iraf+nraf*(k)] and rafep[ibugs[0][l],ibugs[1][l], iraf+nraf*(k)+1]:
-                        idebraf = iraf + nraf*(k)
+        idebraf = np.where(np.logical_and(~rafep_dim, np.roll(rafep_dim, -1)))[0]
+        ifinraf = np.where(np.logical_and(rafep_dim, ~np.roll(rafep_dim, -1)))[0]
 
-                    if rafep[ibugs[0][l], ibugs[1][l], iraf+nraf*(k)] and not rafep[ibugs[0][l],ibugs[1][l], iraf+nraf*(k)+1]:
-                        ifinraf = iraf + nraf*(k)
+        if idebraf.size != 0 and ifinraf.size != 0 and idebraf[0] < ifinraf[0] and np.any(ep_dim == 0):
+            iobj += 1
+            dim_arr[iobj:] = dim_arr[iobj + 1:]
+            dim_arr_f[iobj:] = dim_arr_f[iobj + 1:]
 
-                if idebraf != 0 and ifinraf != 0 and idebraf < ifinraf and iflu == 1:
-                    iobj += 1
+        if idebraf.size != 0 and ifinraf.size != 0 and idebraf[0] > ifinraf[0] and np.any(ep_dim == 1):
+            iobj += 1
+            dim_arr[iobj:] = dim_arr[iobj + 1:]
 
-                    for ii in range(iobj, nobjmax-1):
+        dim_arr[nobjmax - 1] = dim_arr[iobj]
+        dim_arr_f[nobjmax - 1] = dim_arr_f[iobj]
 
-                        dimi[ii,ibugs[0][l], ibugs[1][l]] = dimi[ii+1,ibugs[0][l], ibugs[1][l]]
-                        dimf[ii,ibugs[0][l], ibugs[1][l]] = dimf[ii+1,ibugs[0][l], ibugs[1][l]] 
-                    iobj -= 1
-
-                if idebraf != 0 and ifinraf != 0 and idebraf > ifinraf and isol == 1:
-                    iobj += 1
-                    for ii in range(iobj, nobjmax-1):
-                        dimi[ii,ibugs[0][l],ibugs[1][l]] = dimi[ii+1,ibugs[0][l],ibugs[1][l]]
-                    
-                    iobj -= 1
-                    for ii in range(iobj, nobjmax-1):
-                        dimf[ii,ibugs[0][l],ibugs[1][l]] = dimf[ii+1,ibugs[0][l],ibugs[1][l]]
-
-                idebraf = 0
-                ifinraf = 0
-                iflu = 0
-                isol = 0
-
-    return dimi, dimf
-                
+    return dimi, dimf               
